75e3849dcd014c719a24ae73dd4b74361c1124b3
jerit6490 <jerit6490@2Q3H0Q2>
2022-09-15 13:41:31 -0400

    Add project files.

---
+++ Array/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Title = "Arrays";
+string[] cars = new string[3] {"BMW", "Ford", "Opel"};
+int[,] coords = new int[2,3] {{1,2,3}, { 5,4,6}};
+Console.WriteLine("Second Car: " + cars[1]);
+Console.WriteLine("X1,Y1: " + coords[0, 0]);
+Console.WriteLine("X2,Y3: " + coords[1, 2]);
+Console.ReadKey();
